8338b2e1671da4ba642af362b8d1ce4b8a13aef2
mbreton <mbreton@xebia.fr>
2013-07-17 01:16:11 +0200

    Add web ember structure
    Add PhantomJS dependency
    Add Foundation Css framework
    Add WIP : PhantomsJS switching

--- js/app.js
+++ js/app.js
@@ -1,11 +1,67 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+(function() {
+
+	var get = Ember.get, set = Ember.set;
+
+	Ember.Location.registerImplementation('hashbang', Ember.HashLocation.extend({   
+
+	    getURL: function() {
+	        return get(this, 'location').hash.substr(2);
+	    },
+
+	    setURL: function(path) {
+	        get(this, 'location').hash = "!"+path;
+	        set(this, 'lastSetURL', "!"+path);
+	    },
+
+	    onUpdateURL: function(callback) {
+	        var self = this;
+	        var guid = Ember.guidFor(this);
+
+	        Ember.$(window).bind('hashchange.ember-location-'+guid, function() {
+	        Ember.run(function() {
+	            var path = location.hash.substr(2);
+	            if (get(self, 'lastSetURL') === path) { return; }
+
+	            set(self, 'lastSetURL', null);
+
+	            callback(location.hash.substr(2));
+	        });
+	        });
+	    },
+
+	    formatURL: function(url) {
+	        return '#!'+url;
+	    }
+
+	    })
+	);
+
+})();
+
+App.Router.reopen({
+	location: 'hashbang'
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.Router.map(function() {
+  this.route('detail', {path:'detail/:title'});
+});
+
+App.ApplicationController = Em.Controller.extend({
+	menuItems: [
+		{title:"First Detail"},
+		{title:"Second Detail"},
+		{title:"Third Detail"}
+	]
+});
+
+App.DetailRoute = Em.Route.extend({
+	model: function(params) {
+    	return {title:params.title};
+  	},
+
+  	serialize: function(model) {
+		return { title: model.title };
+	}
 });
--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 var express = require('express');
+var phantom = require('node-phantom');
+var port = process.env.PORT || 5000;
 
 
 var app = express();
 
 app.configure(function () {
     app.use(express.bodyParser());
-    app.use(express.logger());
+    //app.use(express.static(__dirname));
 });
 
-var port = process.env.PORT || 5000;
-
-app.use(express.static(__dirname));
+app.get('/', function(req, res){
+  // If there is _escaped_fragment_ option, it means we have to
+  // generate the static HTML that should normally return the Javascript
+  if(typeof(req.query._escaped_fragment_) !== "undefined") {
+  	console.log('Bot : '+req.query._escaped_fragment_);
+    /*phantom.create(function(err, ph) {
+      return ph.createPage(function(err, page) {
+        // We open phantomJS at the proper page.
+        return page.open("https://localhost:3000/#!" + req.query._escaped_fragment_, function(status) {
+          return page.evaluate((function() {
+            // We grab the content inside <html> tag...
+            return document.getElementsByTagName('html')[0].innerHTML;
+          }), function(err, result) {
+            // ... and we send it to the client.
+            res.send(result);
+            return ph.exit();
+          });
+        });
+      });
+    });*/
+  }
+  else{
+    // If there is no _escaped_fragment_, we return the normal index template.
+    console.log('Bot : '+eq.query._escaped_fragment_);
+    res.render('index');
+  }
+});
 
 app.listen(port);
 console.log('Listening on port '+port+'...');
